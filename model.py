fa302c376954ec5fc604053e1afe1d6c1c9de231
Shree Madan <smadan@olin.edu>
2023-10-23 05:02:33 -0400

    cleaning up code and adding comments

--- model.py
+++ model.py
@@ -1,23 +1,86 @@
+""" model.py
+This file defines the Model class, which represents the overall model in simulation. It handles
+generating all the molecules and then running the simulation on each until they are all trapped or
+captured.
+"""
 from molecule import Molecule
 
 class Model:
-    def __init__(self, num_molecules):
+    """
+    Initialize a Model instance.
+
+    Attributes:
+        num_molecules: an integer representing the total number of molecules in the simulation
+        molecules: a list containing all active Molecules objects in the simulation
+        trapped: an integer representing the number of molecules trapped in the simulation
+        lost: an integer representing the number of molecules lost in the simulation
+        iterations: an integer representing the number of timesteps in the simulation
+        captured_start_latitudes: a list representing the starting latitudes of all molecules which
+        get trapped
+    """
+    def __init__(self, num_molecules=1000, radius=1738e3, mass=2.988e-26, gravity=1.623):
+        """
+        Initialize a Molecule instance.
+
+        Args: 
+            num_molecules: an integer representing the total number of molecules in the simulation
+            radius: an integer representing the radius of the planetary object in the model
+            mass: an integer representing the molecular mass of the chosen molecule in the model
+            gravity: an integer representing the gravity of the planetary object in the model
+
+        Returns: N/A
+        """
         self.num_molecules = num_molecules
-        self.molecules = [Molecule() for _ in range(num_molecules)]
+
+        # Creates a list of Molecule objects corresponding to the num_molecules chosen.
+        self.molecules = [Molecule(radius, mass, gravity) for _ in range(num_molecules)]
+
+        # Trapped and lost are incremented as molecules enter either state
         self.trapped = 0
         self.lost = 0
 
+        # Every time the simulation runs the iterations value is incremented
+        self.iterations = 0
+        self.captured_start_latitudes = []
+
+
     def run_simulation(self):
+        """
+        Run the simulation
+
+        Args: N/A
+
+        Returns: N/A
+        """
+        # The while loop is set so that the simulation will run until all molecules are either
+        # trapped or lost.
         while self.molecules:
+            # Increment the iterations
+            self.iterations += 1
+
+            # During each timestep, each molecule goes through the hop function
             for molecule in self.molecules:
                 molecule.hop()
+
+                # After the molecule hops and the state is updated, it checks whether the state is
+                # lost or trapped and incremented the appropriate integer.
                 if molecule.state == "Lost":
                     self.lost += 1
                 if molecule.state == "Trapped":
                     self.trapped += 1
+                    self.captured_start_latitudes.append(molecule.start_lat)
 
-            # Remove molecules that are either lost or trapped
-            self.molecules = [molecule for molecule in self.molecules if molecule.state not in ["Lost", "Trapped"]]
+            # self.molecules only keeps the molecules which are still hopping. This ensures that
+            # once a molecule reaches a state where it shouldn't move anymore the simulation
+            # doesn't continue to use it.
+            self.molecules = [
+                molecule for molecule in self.molecules
+                if molecule.state == "Hopping"]
 
-        print(f"Captured: {self.trapped}, Lost: {self.lost}, Total: {self.num_molecules}")
-        print(f"Percent captured: {(self.trapped / self.num_molecules) * 100}%")
+        # Once all the molecules are trapped or lost, these statements return a few statistics
+        # about the number of trapped, lost, and iterations as well as calculating the percent of
+        # molecules which were captured.
+        print(
+            f"Captured: {self.trapped}, Lost: {self.lost}, Total iterations: {self.iterations}")
+        print(
+            f"Percent captured: {(self.trapped / self.num_molecules) * 100}%")
--- molecule.py
+++ molecule.py
@@ -1,11 +1,15 @@
+""" molecule.py
+This file defines the Molecule class, which represents an individual molecule in the simulation. It
+contains methods for handling hopping including calculating coordinates and updates values,
+checking loss and capture probabilities.
+"""
 import numpy as np
 
+# Constants taken from Butler 97
 TAU = 2 * 10 ** 4
 BOLTZMANN = 1.38e-23
-MASS = 2.988e-26
-GRAVITY = 1.623
-RADIUS_MOON = 1738e3
 
+# Capture percentages taken from Watson et al. 61
 CAPTURE_PERCENTAGES = {
     "0": 0,
     "10": 0,
@@ -20,56 +24,181 @@ CAPTURE_PERCENTAGES = {
 }
 
 class Molecule:
-    def __init__(self):
-        self.radius = RADIUS_MOON
+    """
+    Initialize a Molecule instance.
+
+    Attributes:
+        inclination: an integer representing the inclination angle of the molecule
+        azimuth: an integer representing the azimuth angle of the molecule
+        start_lat: an integer representing the latitude at which the molecule began
+        state: a string representing the current state of the molecule
+    """
+
+    def __init__(self, radius, mass, gravity):
+        """
+        Initialize a Molecule instance (defaults to H20 molecules on the Moon).
+
+        Args: 
+            radius: an integer representing the radius of the planetary object in the model
+            mass: an integer representing the molecular mass of the chosen molecule in the model
+            gravity: an integer representing the gravity of the planetary object in the model
+
+        Returns: N/A
+        """
+        # A randomly generated inclination angle from 0 to pi
         self.inclination = np.random.uniform(
-            0, np.pi)  # Inclination angle from 0 to pi
-        # Azimuth angle from 0 to 2*pi
+            0, np.pi)
+        # A randomly generated azimuth angle from 0 to 2*pi
         self.azimuth = np.random.uniform(0, 2 * np.pi)
-        self.start_lat = 90 - np.degrees(self.inclination)
+
+        # These three constants can be modified to change the simulation to different environments
+        # (ie. different molecular mass or planetary environmental factors)
+        self.radius = radius
+        self.mass = mass
+        self.gravity = gravity
+
+        # Start latitude calculated based on inclination. This was done to be used as a statistic
+        # in later analysis. The absolute value is then taken to handle negative latitudes and
+        # rounded to the tens place.
+        self.start_lat = round(abs(90 - np.degrees(self.inclination)), -1)
+
+        # self.state is used to track whether or not a molecule is still hopping.
+        # It can also be set to "Lost" or "Trapped" depending on how a molecule exited the model.
         self.state = "Hopping"  # Default state is "Hopping"
 
     def hop(self):
+        """
+        Simulate one hop for molecule.
+
+        Args: N/A
+
+        Returns: N/A
+        """
+        # These first few lines are used to calculate variables which will be needed as inputs into
+        # later functions.
         velocity = self.calculate_velocity()
         hop_time = self.calculate_hop_time(velocity)
         hop_distance = velocity * hop_time / 2
+
+        # This runs the check on whether or not a molecule is lost. If it is it's no longer a part
+        # of the simulation and the function returns prior to doing any calculations on future
+        # coordinates.
         self.check_for_loss(hop_time)
         if self.state == "Lost":  # Set state to "Lost"
             return
+
+        # Once a molecule pass the loss check, the function is run to update the coordinates based
+        # on the hop distance.
         self.update_coordinates(hop_distance)
+
+        # After coordinates are updates, the check on capture is run using the new coordinates.
         self.check_for_capture()
 
     def check_for_loss(self, hop_time):
+        """
+        Calculates probability of loss and checks whether a molecule is lost.
+
+        Args: 
+            hop_time: An integer representing the hop time based on velocity and planetary gravity.
+
+        Returns: N/A
+        """
+        # This equation was provided in both Butler papers to use in calculates of the loss
+        # probability. After this calculation is performed, a random number between 0 and 1 is
+        # generated and assigned to epsilon.
         p_loss = 1 - np.exp(-hop_time / TAU)
         epsilon = np.random.rand()
+
+        # If epsilon is less than what the loss probability is, the molecule is considered lost and
+        # the state is updated accordingly.
         if epsilon < p_loss:
             self.state = "Lost"  # Loss happened
 
     def update_coordinates(self, distance):
+        """
+        Calculate new coordinates after hop.
 
-        scale = distance/RADIUS_MOON
-        
-        # Generate a random azimuth change in the range [0, 2*pi]
+        Args:
+            distance: An integer repesenting the hop distance based on velocity and hop time.
+
+        Returns: N/A
+        """
+        # The way I decided to implement the coordinates updating was using a scale factors.
+        # After calculating the distance, which is an input to this function, it's taken and
+        # divided by the circumference of the planet (2 * pi * r) to give a scale factor for the
+        # angle changes.
+
+        # It's implemented as just the distance divided by radius because it would need to be
+        # multiplied by (2 * pi) for the angle range so that it cancelled out.
+        scale = distance/self.radius
+
+        # Generate a random azimuth change in the range [0, ((distance/circumference) * 2 * pi)]
         azimuth_change = np.random.uniform(0, scale)
-        
-        # Generate a random inclination change in the range [-pi/2, pi/2]
+
+        # Generate a random inclination change in the range [0, ((distance/circumference) * pi)]
         inclination_change = np.random.uniform(0, scale/2)
-        
+
+        # Creating variables of the updated azimuth and inclination after adding the randomly
+        # generated changes.
         new_azimuth = self.azimuth + azimuth_change
         new_inclination = self.inclination + inclination_change
-        # Update azimuth and inclination based on the changes
+
+        # By using mod, it ensures the angles wrap around and don't grow infinitely larger.
         self.azimuth = new_azimuth % (2 * np.pi)
-        self.inclination = new_inclination % (np.pi/2)
-        
+        self.inclination = new_inclination % (np.pi)
+
     def calculate_velocity(self):
-        return np.sqrt(3 * BOLTZMANN * 500 / MASS)
+        """
+        Calculate velocity.
+
+        Args: N/A
+
+        Returns: 
+            An integer representing the velocity based on temperature and molecular mass.
+        """
+        # This is the equation given in Butler 93 for calculating velocity. Although it's a
+        # constant here, this function could be modified to accomodate a variable temperature which
+        # right now is set to be a constant 500 K.
+        return np.sqrt(3 * BOLTZMANN * 500 / self.mass)
 
     def calculate_hop_time(self, velocity):
-        return (2 ** 0.5) * velocity / GRAVITY
+        """
+        Calculate velocity.
+
+        Args: N/A
+
+        Returns: 
+            An integer representing the hop time based on velocity and planetary gravity.
+        """
+        # This is the equation given in Butler 93 for calculating hop time. Although it's a
+        # constant here, this function could be modified to accomodate a variable gravity equation
+        # similar to what was used in Butler 97.
+        return (2 ** 0.5) * velocity / self.gravity
 
     def check_for_capture(self):
-        lat_in_degrees = 90 - np.degrees(self.inclination)
+        """
+        Calculates probability of capture and checks whether a molecule is captured.
+
+        Args: N/A
+
+        Returns: N/A
+        """
+        # Converting inclination to latitude using equation provided by Butler 97.
+        # The absolute value is then taken to handle negative latitudes and rounded to the tens
+        # place to associate with capture percentages.
+        lat_in_degrees = round(abs(90 - np.degrees(self.inclination)), -1)
+
+        # Converting integer to string to match dictionary keys
         latitude_bin = str(int(lat_in_degrees))
+
+        # The latitude is then used to retreve the capture percentage for that 10 degree area.
+        # This is the percentage of surface area which is cold enough to trap a molecule and we
+        # assume that if a molecule enters a capture area it cannot leave. If the latitude isn't
+        # found in the dictionary, it defaults to 0.
         capture_percentage = CAPTURE_PERCENTAGES.get(latitude_bin, 0)
+
+        # A number between 0 and 1 is generated and then converted to 0 to 100 to check against the
+        # percentages. If the randomly generate number is less than the capture percentage the
+        # molecule is considered trapped and the state is updated to reflect that.
         if np.random.rand() * 100 < capture_percentage:
             self.state = "Trapped"
